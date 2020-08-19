f449299752437e06673fe07febfac5837104af29
v-tawe <v-tawe@microsoft.com>
2020-08-19 18:00:16 +0800

    Spring Cloud init

---
+++ 4-Spring/SpringCloud/code/cloud-provider-payment8001/src/main/java/com/tawe/springcloud/PaymentMain8001.java
@@ -0,0 +1,19 @@
+package com.tawe.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName com.tawe.springcloud.PaymentMain8001
+ * @Description TODO
+ * @Author davidt
+ * @Date 8/19/2020 5:39 PM
+ * @Version 1.0
+ **/
+
+@SpringBootApplication
+public class PaymentMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class, args);
+    }
+}
