df2494445414d092d2fe27cd88ca7d9c9c63418c
v-tawe <v-tawe@microsoft.com>
2020-09-11 00:12:23 +0800

    Spring + SpringMVC + Mybatis 整合 - fix druid & dispatchServlet issues

--- 4-Spring/SpringMVC/code/idea/crowdfunding/crowdfunding-web/src/main/java/com/tawe/crowdfunding/controller/IndexController.java
+++ 4-Spring/SpringMVC/code/idea/crowdfunding/crowdfunding-web/src/main/java/com/tawe/crowdfunding/controller/IndexController.java
@@ -1,5 +1,7 @@
 package com.tawe.crowdfunding.controller;
 
+import com.tawe.crowdfunding.service.IndexService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -13,8 +15,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
+    @Autowired
+    private IndexService indexService;
+
     @RequestMapping("/index")
     public String index() {
-        return "index";
+        return indexService.index();
     }
 }
--- 4-Spring/SpringMVC/code/idea/crowdfunding/crowdfunding-web/src/main/java/com/tawe/crowdfunding/service/IndexService.java
+++ 4-Spring/SpringMVC/code/idea/crowdfunding/crowdfunding-web/src/main/java/com/tawe/crowdfunding/service/IndexService.java
@@ -4,4 +4,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class IndexService {
+    public String index() {
+        return "index";
+    }
 }
